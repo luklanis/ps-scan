bb551c5c72017fe620aca5b0943f50642647b5af
Lukas Landis <landis.lukas@gmail.com>
2012-02-02 10:09:48 +0100

    DTA file creator completed but not tested yet

--- android/src/ch/luklanis/esscan/paymentslip/DTAFileCreator.java
+++ android/src/ch/luklanis/esscan/paymentslip/DTAFileCreator.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import ch.luklanis.esscan.PreferencesActivity;
+import ch.luklanis.esscan.R;
 import ch.luklanis.esscan.history.HistoryItem;
 
 import android.app.Activity;
@@ -47,36 +49,132 @@ public class DTAFileCreator {
 	 * </ul>
 	 */
 	public CharSequence buildDTA(List<HistoryItem> historyItems) {
-		StringBuilder historyText = new StringBuilder(1000);
+		StringBuilder dtaText = new StringBuilder(1000);
 		
 		String today = getDateFormated(new Date(System.currentTimeMillis()));
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
 		
-		String iban = prefs.getString(PreferencesActivity.KEY_IBAN, "");
+		String iban = prefs.getString(PreferencesActivity.KEY_IBAN, "").replaceAll("\\s", "");
+		
+		String[] ownAddress = prefs.getString(PreferencesActivity.KEY_ADDRESS, "").split("\\r\\n");
+		
+		float totalAmount = 0;
 
 		if(iban == ""){
 			return "";
 		}
-
-		for (int i = 0; i < historyItems.size(); i++) {
-			HistoryItem historyItem = historyItems.get(i);
-			
-			String clearing = String.valueOf((Integer.parseInt(iban.substring(4, 9))));
-
-			// HEADER
-			historyText
-			.append("01")
-			.append(getExecutionDateFormated())
-			.append(spacePaddedEnd("", 12))
-			.append(padded("", '0', 5, true))
-			.append(nullToEmpty(today))
-			.append(spacePaddedEnd(clearing, 7))
-			.append(padded("", 'X', 5, true))
-			.append(padded(String.valueOf(i + 1), '0', 5, false))
-			.append("82600"); 
-
+		
+		String clearing = String.valueOf((Integer.parseInt(iban.substring(4, 9))));
+		
+		List<EsrResult> esrResults = new ArrayList<EsrResult>();
+		
+		for (HistoryItem historyItem : historyItems) {
+			if(historyItem.getResult().getCurrency() == "CHF"){
+				esrResults.add(historyItem.getResult());
+			}
 		}
-		return historyText;
+
+		for (int i = 0; i < esrResults.size(); i++) {
+			EsrResult esrResult = esrResults.get(i);
+			
+			String currency = esrResult.getCurrency();
+			
+			String account = esrResult.getAccount().replaceAll("-", "");
+			
+			String[] address = esrResult.getAddress() != null ? esrResult.getAddress().split("\\r\\n"):new String[0];
+			
+			CharSequence paddedSequenz = padded(String.valueOf(i + 1), '0', 5, false);
+			
+			String amount = esrResult.getAmount().replace('.', ',');
+			
+			totalAmount += Float.parseFloat(esrResult.getAmount());
+
+			// HEADER for ESR
+			dtaText
+			.append("01")	// Segment number
+			.append(getExecutionDateFormated())	// desired execution date
+			.append(spacePaddedEnd("", 12))	// Clearing number of the target bank (not needed for ESR payments)
+			.append(padded("", '0', 5, true))	// Sequenz number (has to be 5 x 0)
+			.append(nullToEmpty(today))	// creation date
+			.append(spacePaddedEnd(clearing, 7))	// own clearing number
+			.append(padded("", 'X', 5, true))	// identification number
+			.append(paddedSequenz)	// sequenz number
+			.append("82600");	// transaction type (ESR = 826), payment type (ESR = 0) and a flag (always 0)
+			
+			// ESR
+			dtaText
+			.append(padded("", 'X', 5, true))	// identification number (again)
+			.append("WZ0000")	// transaction number part 1
+			.append(paddedSequenz)	// transaction number part 2
+			.append(spacePaddedEnd(iban, 24))	// own IBAN
+			.append(spacePaddedEnd("", 6))	// Valuta (Blanks in ESR)
+			.append(currency)
+			.append(spacePaddedEnd(amount, 12))
+			.append(spacePaddedEnd("", 14))	// Reserve
+			.append("02")	// Begin Segment 02
+			.append(spacePaddedEnd(ownAddress[0], 20))
+			.append(spacePaddedEnd(ownAddress[1], 20));
+			
+			if(ownAddress.length > 2){
+				dtaText.append(spacePaddedEnd(ownAddress[2], 20));
+			}
+			if(ownAddress.length > 3){
+				dtaText.append(spacePaddedEnd(ownAddress[3], 20));
+			}
+			dtaText.append(spacePaddedEnd("", 46));	// Reserve
+			
+			dtaText
+			.append("03")	// Begin Segment 03
+			.append("/C/")	// Account begin
+			.append(padded(account, '0', 9, false));	// Account
+			
+			if(address.length > 0){
+				dtaText.append(spacePaddedEnd(address[0], 20));
+			}
+			if(address.length > 1){
+				dtaText.append(spacePaddedEnd(address[1], 20));
+			}
+			if(address.length > 2){
+				dtaText.append(spacePaddedEnd(address[2], 20));
+			}
+			if(address.length > 3){
+				dtaText.append(spacePaddedEnd(address[3], 20));
+			}
+			
+			if(account.length() > 5){
+				dtaText.append(padded(esrResult.getReference(), '0', 27, false));	// Referenz number
+			}
+			else{
+				dtaText.append(spacePaddedEnd(esrResult.getReference(), 27));	// Refernz number (with 5 digits account)
+				Log.w(TAG, "account only 5 digits long -> this will not work!");
+			}
+			
+			dtaText.append("  ")	// ESR Checksum (only with 5 digits, which is not supported)
+			.append(spacePaddedEnd("", 5));	// Reserve
+		}
+		
+		// HEADER for Total Record
+		dtaText
+		.append("01")	// Segment number
+		.append(getExecutionDateFormated())	// desired execution date
+		.append(spacePaddedEnd("", 12))	// Clearing number of the target bank (not needed for ESR payments)
+		.append(padded("", '0', 5, true))	// Sequenz number (has to be 5 x 0)
+		.append(nullToEmpty(today))	// creation date
+		.append(spacePaddedEnd(clearing, 7))	// own clearing number
+		.append(padded("", 'X', 5, true))	// identification number
+		.append(padded(String.valueOf(esrResults.size() + 1), '0', 5, false))	// sequenz number
+		.append("89000");	// transaction type (Total Record = 890), payment type (ESR = 0) and a flag (always 0)
+		
+		String totalAmountTemp = String.valueOf(totalAmount).replace('.', ',');
+		int indexOfDecimal = totalAmountTemp.indexOf(',');
+		totalAmountTemp = totalAmountTemp.substring(indexOfDecimal + 1, indexOfDecimal + 3);
+		
+		// Total Record
+		dtaText
+		.append(spacePaddedEnd(totalAmountTemp, 16))
+		.append(spacePaddedEnd("", 59)); // Reserve
+		
+		return dtaText;
 	}
 
 	public static Uri saveDTAFile(String history) {
@@ -125,16 +223,30 @@ public class DTAFileCreator {
 		expectedCalendar.setTime(now);
 		expectedCalendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(day));
 
-		if(expectedCalendar.before(nowCalendar)){
-			int month = expectedCalendar.get(Calendar.MONTH);
-
-			if(month < Calendar.DECEMBER){
-				expectedCalendar.set(Calendar.MONTH, month + 1);
-			}
-			else{
-				expectedCalendar.set(Calendar.MONTH, Calendar.JANUARY);
-				expectedCalendar.set(Calendar.YEAR, (expectedCalendar.get(Calendar.YEAR) + 1));
-			}
+		if(!expectedCalendar.after(nowCalendar)){
+//			int month = expectedCalendar.get(Calendar.MONTH);
+//
+//			if(month < Calendar.DECEMBER){
+//				expectedCalendar.set(Calendar.MONTH, month + 1);
+//			}
+//			else{
+//				expectedCalendar.set(Calendar.MONTH, Calendar.JANUARY);
+//				expectedCalendar.set(Calendar.YEAR, (expectedCalendar.get(Calendar.YEAR) + 1));
+//			}
+			expectedCalendar.add(Calendar.MONTH, 1);
+		}
+		
+		int dayOfWeek = expectedCalendar.get(Calendar.DAY_OF_WEEK);
+		
+		switch(dayOfWeek){
+		case Calendar.SATURDAY:
+			expectedCalendar.add(Calendar.DAY_OF_WEEK, 2);
+			break;
+		case Calendar.SUNDAY:
+			expectedCalendar.add(Calendar.DAY_OF_WEEK, 1);
+			break;
+		default:
+			break;
 		}
 
 		return sdf.format(expectedCalendar.getTime());
