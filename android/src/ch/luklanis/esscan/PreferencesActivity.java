806629c871b3ebdc27e9d7292d9cecd43d73ed5a
Lukas Landis <lukas.landis@kaba.com>
2012-02-20 11:03:37 +0100

    fix preferences

--- android/src/ch/luklanis/esscan/PreferencesActivity.java
+++ android/src/ch/luklanis/esscan/PreferencesActivity.java
@@ -39,141 +39,150 @@ import android.preference.PreferenceManager;
  * The code for this class was adapted from the ZXing project: http://code.google.com/p/zxing
  */
 public class PreferencesActivity extends PreferenceActivity implements
-  OnSharedPreferenceChangeListener {
-  
-  // Preference keys not carried over from ZXing project
-  public static final String KEY_SOURCE_LANGUAGE_PREFERENCE = "preferences_source_language";
-  public static final String KEY_CHARACTER_WHITELIST = "preferences_character_whitelist";
-  public static final String KEY_ONLY_MACRO_FOCUS = "preferences_only_macro_focus";
-  public static final String KEY_ENABLE_TORCH = "preferences_enable_torch";
-  public static final String KEY_ADDRESS = "preferences_address";
-  public static final String KEY_IBAN = "preferences_iban";
-  public static final String KEY_EXECUTION_DAY = "preferences_execution_day";
-  
-  // Preference keys carried over from ZXing project
-  public static final String KEY_REVERSE_IMAGE = "preferences_reverse_image";
-  public static final String KEY_PLAY_BEEP = "preferences_play_beep";
-  public static final String KEY_VIBRATE = "preferences_vibrate";
-  
-  public static final String KEY_HELP_VERSION_SHOWN = "preferences_help_version_shown";
-  
-  //private CheckBoxPreference checkBoxPreferenceContinuousPreview;
-  private ListPreference listPreferenceSourceLanguage;
-  private EditTextPreference editTextPreferenceCharacterWhitelist;
-  
-  private static SharedPreferences sharedPreferences;
-  
-  /**
-   * Set the default preference values.
-   * 
-   * @param Bundle
-   *            savedInstanceState the current Activity's state, as passed by
-   *            Android
-   */
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    addPreferencesFromResource(R.xml.preferences);
-    
-    sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-    
-    listPreferenceSourceLanguage = (ListPreference) getPreferenceScreen().findPreference(KEY_SOURCE_LANGUAGE_PREFERENCE);
-    editTextPreferenceCharacterWhitelist = (EditTextPreference) getPreferenceScreen().findPreference(KEY_CHARACTER_WHITELIST);
-  }
-  
-  /**
-   * Interface definition for a callback to be invoked when a shared
-   * preference is changed. Sets summary text for the app's preferences. Summary text values show the
-   * current settings for the values.
-   * 
-   * @param sharedPreferences
-   *            the Android.content.SharedPreferences that received the change
-   * @param key
-   *            the key of the preference that was changed, added, or removed
-   */
-  @Override
-  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-      String key) {    
-    // Update preference summary values to show current preferences
-    if(key.equals(KEY_SOURCE_LANGUAGE_PREFERENCE)) {
-      
-      // Set the summary text for the source language name
-      listPreferenceSourceLanguage.setSummary(LanguageCodeHelper.getOcrLanguageName(getBaseContext(), sharedPreferences.getString(key, "deu")));
-      
-      // Retrieve the character whitelist for the new language
-      String whitelist = OcrCharacterHelper.getWhitelist(sharedPreferences, listPreferenceSourceLanguage.getValue());
-      
-      // Save the character whitelist to preferences
-      sharedPreferences.edit().putString(KEY_CHARACTER_WHITELIST, whitelist).commit();
-      
-      // Set the whitelist summary text
-      editTextPreferenceCharacterWhitelist.setSummary(whitelist);
+OnSharedPreferenceChangeListener {
 
-    } else if (key.equals(KEY_CHARACTER_WHITELIST)) {
-      
-      // Save a separate, language-specific character blacklist for this language
-      OcrCharacterHelper.setWhitelist(sharedPreferences, 
-          listPreferenceSourceLanguage.getValue(), 
-          sharedPreferences.getString(key, OcrCharacterHelper.getDefaultWhitelist(listPreferenceSourceLanguage.getValue())));
-      
-      // Set the summary text
-      editTextPreferenceCharacterWhitelist.setSummary(sharedPreferences.getString(key, OcrCharacterHelper.getDefaultWhitelist(listPreferenceSourceLanguage.getValue())));
-      
-    } else if (key.equals(KEY_IBAN)){
-    	String iban = sharedPreferences.getString(key, "");
-    	
-    	int warning = DTAFileCreator.validateIBAN(iban);
-    	if (warning != 0){
-    		setOKAlert(warning);
-    	}
-    }else if (key.equals(KEY_ADDRESS)){
-    	String address = sharedPreferences.getString(key, "");
-    	
-    	int warning = DTAFileCreator.validateAddress(address);
-    	if (warning != 0){
-    		setOKAlert(warning);
-    	}
-    }
-  }
-  
-  /**
-   * Sets up initial preference summary text
-   * values and registers the OnSharedPreferenceChangeListener.
-   */
-  @Override
-  protected void onResume() {
-    super.onResume();
-    // Set up the initial summary values
-    listPreferenceSourceLanguage.setSummary(LanguageCodeHelper.getOcrLanguageName(getBaseContext(), sharedPreferences.getString(KEY_SOURCE_LANGUAGE_PREFERENCE, "deu")));
-    editTextPreferenceCharacterWhitelist.setSummary(sharedPreferences.getString(KEY_CHARACTER_WHITELIST, OcrCharacterHelper.getDefaultWhitelist(listPreferenceSourceLanguage.getValue())));
-    
-    // Set up a listener whenever a key changes
-    getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-  }
+	// Preference keys not carried over from ZXing project
+	public static final String KEY_SOURCE_LANGUAGE_PREFERENCE = "preferences_source_language";
+	public static final String KEY_CHARACTER_WHITELIST = "preferences_character_whitelist";
+	public static final String KEY_ONLY_MACRO_FOCUS = "preferences_only_macro_focus";
+	public static final String KEY_ENABLE_TORCH = "preferences_enable_torch";
+	public static final String KEY_ADDRESS = "preferences_address";
+	public static final String KEY_IBAN = "preferences_iban";
+	public static final String KEY_EXECUTION_DAY = "preferences_execution_day";
 
-  /**
-   * Called when Activity is about to lose focus. Unregisters the
-   * OnSharedPreferenceChangeListener.
-   */
-  @Override
-  protected void onPause() {
-    super.onPause();
-    getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-  }
-  
-  private void setOKAlert(int id){
-	  AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	  builder.setMessage(id);
-	  builder.setPositiveButton(R.string.button_ok, new OnClickListener() {
-		  public void onClick(DialogInterface dialog, int which) {
-			  reload();
-		  }
-	  });
-	  builder.show();
-  }
-  
-  private void reload(){
-      startActivity(getIntent()); 
-      finish();
- }
+	// Preference keys carried over from ZXing project
+	public static final String KEY_REVERSE_IMAGE = "preferences_reverse_image";
+	public static final String KEY_PLAY_BEEP = "preferences_play_beep";
+	public static final String KEY_VIBRATE = "preferences_vibrate";
+
+	public static final String KEY_HELP_VERSION_SHOWN = "preferences_help_version_shown";
+
+	//  private ListPreference listPreferenceSourceLanguage;
+	private EditTextPreference editTextPreferenceCharacterWhitelist;
+
+	private static SharedPreferences sharedPreferences;
+
+	/**
+	 * Set the default preference values.
+	 * 
+	 * @param Bundle
+	 *            savedInstanceState the current Activity's state, as passed by
+	 *            Android
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+		//    listPreferenceSourceLanguage = (ListPreference) getPreferenceScreen().findPreference(KEY_SOURCE_LANGUAGE_PREFERENCE);
+		editTextPreferenceCharacterWhitelist = (EditTextPreference) getPreferenceScreen().findPreference(KEY_CHARACTER_WHITELIST);
+	}
+
+	/**
+	 * Interface definition for a callback to be invoked when a shared
+	 * preference is changed. Sets summary text for the app's preferences. Summary text values show the
+	 * current settings for the values.
+	 * 
+	 * @param sharedPreferences
+	 *            the Android.content.SharedPreferences that received the change
+	 * @param key
+	 *            the key of the preference that was changed, added, or removed
+	 */
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {    
+		//    // Update preference summary values to show current preferences
+		//    if(key.equals(KEY_SOURCE_LANGUAGE_PREFERENCE)) {
+		//      
+		//      // Set the summary text for the source language name
+		//      listPreferenceSourceLanguage.setSummary(LanguageCodeHelper.getOcrLanguageName(getBaseContext(), sharedPreferences.getString(key, "deu")));
+		//      
+		//      // Retrieve the character whitelist for the new language
+		//      String whitelist = OcrCharacterHelper.getWhitelist(sharedPreferences, listPreferenceSourceLanguage.getValue());
+		//      
+		//      // Save the character whitelist to preferences
+		//      sharedPreferences.edit().putString(KEY_CHARACTER_WHITELIST, whitelist).commit();
+		//      
+		//      // Set the whitelist summary text
+		//      editTextPreferenceCharacterWhitelist.setSummary(whitelist);
+		//
+		//    } else if (key.equals(KEY_CHARACTER_WHITELIST)) {
+		//  
+		//      // Save a separate, language-specific character blacklist for this language
+		//      OcrCharacterHelper.setWhitelist(sharedPreferences, 
+		//          listPreferenceSourceLanguage.getValue(), 
+		//          sharedPreferences.getString(key, OcrCharacterHelper.getDefaultWhitelist(listPreferenceSourceLanguage.getValue())));
+		//      
+		//      // Set the summary text
+		//      editTextPreferenceCharacterWhitelist.setSummary(sharedPreferences.getString(key, OcrCharacterHelper.getDefaultWhitelist(listPreferenceSourceLanguage.getValue())));
+
+		if (key.equals(KEY_CHARACTER_WHITELIST)) {
+
+			// Save a separate, language-specific character blacklist for this language
+			OcrCharacterHelper.setWhitelist(sharedPreferences, 
+					"deu", 
+					sharedPreferences.getString(key, OcrCharacterHelper.getDefaultWhitelist("deu")));
+
+			// Set the summary text
+			editTextPreferenceCharacterWhitelist.setSummary(sharedPreferences.getString(key, OcrCharacterHelper.getDefaultWhitelist("deu")));
+
+		} else if (key.equals(KEY_IBAN)){
+			String iban = sharedPreferences.getString(key, "");
+
+			int warning = DTAFileCreator.validateIBAN(iban);
+			if (warning != 0){
+				setOKAlert(warning);
+			}
+		}else if (key.equals(KEY_ADDRESS)){
+			String address = sharedPreferences.getString(key, "");
+
+			int warning = DTAFileCreator.validateAddress(address);
+			if (warning != 0){
+				setOKAlert(warning);
+			}
+		}
+	}
+
+	/**
+	 * Sets up initial preference summary text
+	 * values and registers the OnSharedPreferenceChangeListener.
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// Set up the initial summary values
+		//    listPreferenceSourceLanguage.setSummary(LanguageCodeHelper.getOcrLanguageName(getBaseContext(), sharedPreferences.getString(KEY_SOURCE_LANGUAGE_PREFERENCE, "deu")));
+		editTextPreferenceCharacterWhitelist.setSummary(sharedPreferences.getString(KEY_CHARACTER_WHITELIST, OcrCharacterHelper.getDefaultWhitelist("deu")));
+
+		// Set up a listener whenever a key changes
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+
+	/**
+	 * Called when Activity is about to lose focus. Unregisters the
+	 * OnSharedPreferenceChangeListener.
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	private void setOKAlert(int id){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(id);
+		builder.setPositiveButton(R.string.button_ok, new OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				reload();
+			}
+		});
+		builder.show();
+	}
+
+	private void reload(){
+		startActivity(getIntent()); 
+		finish();
+	}
 }
